99e4dd4413c5e85f0e3672c2945a1a768c1d4893
BYUNSUJUNG <susususu07@naver.com>
2020-06-26 23:46:03 +0900

    git init 2020-06-26

--- src/Step05/StarPrint2.java
+++ src/Step05/StarPrint2.java
@@ -10,17 +10,28 @@ public class StarPrint2 {
 		// 몇 번째 까지 출력할 지 입력
 		int iCnt = scan.nextInt();
 		
-		// 반복문
-		for (int i = 0; i < iCnt; i++) { 
+		// 뒤집어진 삼각형
+		for (int i = 0; i < iCnt; i++) { // 0 1 2 3 4
 			for (int j = 0; j < i; j++) { // 공백 출력
 				System.out.print(" ");
 			}
-			/*
-				for (int k = iCnt-i; k < (k*2)-1; k++) {  // 별 출력
-					
-				}
-			*/
-			System.out.println("");
+			
+			for (int k = 0; k < (iCnt-i)*2-1; k++) {  // 별 출력
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+		
+		// 삼각형
+		for (int i = 0; i < iCnt-1; i++) { // 0 1 2 3 
+			for (int j = 0; j < (iCnt-2)-i; j++) { // 공백 출력
+				System.out.print(" ");
+			}
+			
+			for (int k = 0; k < (i*2)+3; k++) {  // 별 출력
+				System.out.print("*");
+			}
+			System.out.println(" ");
 		}
 
 
